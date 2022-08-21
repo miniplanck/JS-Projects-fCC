0533bdf2d4e71922a18738dc9eaee404c4f0c623
miniplanck <73555298+miniplanck@users.noreply.github.com>
2022-08-21 12:21:08 +0100

    Update to the comments

--- Caesars Cipher.js
+++ Caesars Cipher.js
@@ -15,7 +15,7 @@ function rot13(str) {
     if(element.charCodeAt(0)<65 || element.charCodeAt(0)>90){  //If the unicode of the character is not in the required limits, it is not a CAPITAL letter --> nothing happens
       return element;
     }
-    else{ //For CAPITAL letters, we shift the unicode for "A" to 0: subtracting 13 and adding 26 accounts for switches from "A" to "Z". Finally, we account for characters that do not need a shift, by getting %26. Finally, we go back to the original representantion where unicode("A") = 65
+    else{ //For CAPITAL letters, we shift the unicode for "A" to 0: subtracting 13 and adding 26 accounts for switches from "A" to "Z". Finally, we account for characters that do not need a shift, by doing %26. Finally, we go back to the original representantion where unicode("A") = 65
       return String.fromCharCode(((element.charCodeAt(0)-65-13)+26)%26+65)
       };
   }).join("");
