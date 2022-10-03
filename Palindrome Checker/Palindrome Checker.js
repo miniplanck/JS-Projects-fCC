d4e1b219106321a19c263b193587fb53838d6c12
José Pereira <73555298+miniplanck@users.noreply.github.com>
2022-10-03 10:24:03 +0100

    Update Palindrome Checker.js

--- Palindrome Checker/Palindrome Checker.js
+++ Palindrome Checker/Palindrome Checker.js
@@ -1,9 +1,9 @@
 function palindrome(str) {
   let letters = str.toLowerCase().split(/[^a-zA-Z0-9]/i).join('').split(''); //Array with the alphanumeric characters.
   
-  let strFiltered = words.join(''); //Creates a string made of the previous letters.
+  let strFiltered = letters.join(''); //Creates a string made of the previous letters.
   
-  let reversedStr = words.reverse().join('');//The array "letters" is reversed and parsed into a string.
+  let reversedStr = letters.reverse().join('');//The array "letters" is reversed and parsed into a string.
   
   return reversedStr == strFiltered;//This compares the original alphanumeric string with its reversed version. If they are the same, then it is a Palindrome and returns 'true' 
 }
