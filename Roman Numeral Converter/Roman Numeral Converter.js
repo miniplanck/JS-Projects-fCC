67742fe53d6a44e7af671f9b68e4ff60962de5ba
José Pereira <73555298+miniplanck@users.noreply.github.com>
2022-10-20 09:14:42 +0100

    Update Roman Numeral Converter.js

--- Roman Numeral Converter/Roman Numeral Converter.js
+++ Roman Numeral Converter/Roman Numeral Converter.js
@@ -1,7 +1,5 @@
 //Roman Numeral Converter
 
-//Convert the given number into a roman numeral.
-
 function convertToRoman(num) {
  let dic = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",90:"XC",50:"L",40:"XL",10:"X",9:"IX",5:"V",4:"IV",1:"I"}; //Creates a dictionary with the standard Roman numerals
 
